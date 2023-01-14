f752c65ca9063f62fc0ec8c75987922a3cc241e1
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-01-14 10:54:28 +0300

    Completed Assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import {username,city} from "/home/latimore/react-hooks-import-export-lab/src/data/user.js"
 function Home() {
   return (
     <div id="home">
@@ -9,3 +9,4 @@ function Home() {
     </div>
   );
 }
+export default Home
